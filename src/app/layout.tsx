ad83b879db65ad9289d0192670113e909776d9b8
Chris VanGroll <79307697+chrisvangroll@users.noreply.github.com>
2023-12-04 21:52:32 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+        <head>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"/>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
